4d29cc69908390ac679849ae413883c3d3309d0c
Aashish Thapa <aashishthapa520@gmail.com>
2026-02-15 03:18:48 -0600

    feat: add ServerStatusWidget to right sidebar

--- src/components/layout/RightSidebar.tsx
+++ src/components/layout/RightSidebar.tsx
@@ -1,11 +1,13 @@
 import { TrendingWidget } from './TrendingWidget'
 import { FollowSuggestionsWidget } from './FollowSuggestionsWidget'
+import { ServerStatusWidget } from './ServerStatusWidget'
 
 export function RightSidebar() {
   return (
     <div className='space-y-6'>
       <TrendingWidget />
       <FollowSuggestionsWidget />
+      <ServerStatusWidget />
     </div>
   )
 }
